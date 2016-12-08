11e1d545a9dbd539363f3a8150c57b9502d7b3cc
Junius-Wu <727987555@qq.com>
2016-12-08 13:21:12 +0800

    获取选择的pathTuple的index

--- violetproduct-swing/src/main/java/com/horstmann/violet/application/gui/util/wujun/TDVerification/ExistUtil.java
+++ violetproduct-swing/src/main/java/com/horstmann/violet/application/gui/util/wujun/TDVerification/ExistUtil.java
@@ -4,7 +4,8 @@ package com.horstmann.violet.application.gui.util.wujun.TDVerification;
  * Created by jun on 16/12/7.
  */
 public class ExistUtil {
-    static public ExistVerification getExist(String filePath) throws Exception {
-        return new ExistVerification(filePath);
+	private static ExistVerification existVerification;
+    public static ExistVerification getExist(String filePath) throws Exception {
+    	return existVerification = new ExistVerification(filePath);
     }
 }
