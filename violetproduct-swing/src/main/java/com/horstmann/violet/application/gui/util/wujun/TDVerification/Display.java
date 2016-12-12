7ac7e1e10a9c2ea46cee60772b62001cff415dc8
Junius-Wu <727987555@qq.com>
2016-12-12 12:08:16 +0800

    添加输出部分内容

---
+++ violetproduct-swing/src/main/java/com/horstmann/violet/application/gui/util/wujun/TDVerification/Display.java
@@ -0,0 +1,14 @@
+package com.horstmann.violet.application.gui.util.wujun.TDVerification;
+
+public class Display {
+	public static void println(String value) {
+
+		System.out.println(value);
+		
+	}
+	
+	public static void println() {
+		println(" ");
+	}
+}
+
